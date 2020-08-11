162a444cac591999d2ab6fbe2643402038171d58
Mathilda Djamdjian <mathilda.djamdjian@gmail.com>
2020-08-11 17:23:11 +0200

    Crud done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::TvShows::Friends.character,
+    content: "#{Faker::TvShows::Friends.quote}, #{Faker::TvShows::Friends.location}"
+)
+  article.save!
+end
+puts 'Finished!'
